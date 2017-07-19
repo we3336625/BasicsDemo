18a03837cf6e4bd44c231d9ab3899d1866bc1f40
zhangxiang <ex-zhangxiang@xianglin.cn>
2017-07-19 11:33:31 +0800

    单击图片，图片循环显示； 自定义view， circle跟随手指移动

---
+++ CrazyAndroid/app/src/main/java/com/zx/crazyandroid/biz/BaseActivity.java
@@ -0,0 +1,21 @@
+/*
+ *
+ *  * 乡邻小站
+ *  *   *Copyright (c) 2017 XiangLin,Inc.All Rights Reserved.
+ *
+ */
+
+package com.zx.crazyandroid.biz;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 【类功能说明】
+ *
+ * @author zhangxiang
+ * @version v 1.4.8 2017/7/18 下午4:46 XLXZ Exp $
+ * @email ex-zhangxiang@xianglin.cn
+ */
+public class BaseActivity extends AppCompatActivity {
+    String TAG = this.getClass().getSimpleName();
+}
