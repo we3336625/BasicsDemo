c5cf6aefc5ec8e46c1706342704e389402bd9aed
zhangxiang <ex-zhangxiang@xianglin.cn>
2017-07-25 16:59:17 +0800

    calendarView 选择日期

---
+++ CrazyAndroid/app/src/main/java/com/zx/crazyandroid/biz/view/view_group/frame_layout/calendar_view/CalendarViewActivity.java
@@ -0,0 +1,31 @@
+package com.zx.crazyandroid.biz.view.view_group.frame_layout.calendar_view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.zx.crazyandroid.R;
+import com.zx.crazyandroid.biz.BaseActivity;
+
+/**
+ * calendarView 选择日期
+ */
+public class CalendarViewActivity extends BaseActivity {
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_view);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(CalendarViewActivity.this, year + "年" + (month + 1) + "月" + dayOfMonth + "日", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
