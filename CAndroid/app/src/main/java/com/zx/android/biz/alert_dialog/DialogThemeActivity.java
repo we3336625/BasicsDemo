9b46a35504ad50f065fe7cb95cdb07e668009a2d
zhangxiang <ex-zhangxiang@xianglin.cn>
2017-07-27 16:05:17 +0800

    对话框风格的窗口

---
+++ CAndroid/app/src/main/java/com/zx/android/biz/alert_dialog/DialogThemeActivity.java
@@ -0,0 +1,18 @@
+package com.zx.android.biz.alert_dialog;
+
+import android.os.Bundle;
+
+import com.zx.android.R;
+import com.zx.android.biz.BaseActivity;
+
+/**
+ * 对话框风格的窗口
+ */
+public class DialogThemeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_theme);
+    }
+}
